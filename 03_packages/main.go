aa49386dcb8235de8d20b6544ef1d3a58d1a33fd
Mohamed Haneef <haneefaths@gmail.com>
2022-06-07 23:16:04 +0400

    packages

---
+++ 03_packages/main.go
@@ -0,0 +1,10 @@
+package main // contains main function
+import (
+	"fmt"
+	"math"
+)
+
+func main() { // runs by default, no need for function call
+	fmt.Println("hello world!")
+	fmt.Println("Square root of 8 is ", math.Sqrt(8))
+}
