3f42b7ba1f9c6ae437435c4ba759a67d37c3d77f
Mohamed Haneef <haneefaths@gmail.com>
2022-06-07 21:37:02 +0400

    variables and types - listing most of the data types

---
+++ 02_vars/main.go
@@ -0,0 +1,16 @@
+package main
+
+func main() {
+	/* most of the main data types
+	1. string
+	2. bool
+	3. int
+		a. int int8 int16 int32 int64
+		b. uint uint8 uint16 uint32 uint64 uintptr
+	4. byte - same as uint8
+	5. rune - same as uint32
+	6. float32 float64
+	7. complex64 complex128
+	*/
+
+}
